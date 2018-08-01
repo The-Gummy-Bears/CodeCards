99e2301766588490ef7151b25e4df54eb596b6e7
Nicole Houston <nicole.houston@cox.net>
2018-07-31 19:27:53 -0700

    user can delete category cards which in turn will delete info cards

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -55,13 +55,11 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  // deleteCategory: function (req, res) {
-  //   console.log(req.body)
-  //   const updateCategories = { $pull: { levelOne: {category: req.body.category}, categories: req.body.category }};
-  //   console.log(updateCategories);
-  //   db.User
-  //     .update({ username: req.body.username }, updateCategories, { multi: true })
-  //     .then(dbModel => res.json(dbModel))
-  //     .catch(err => res.status(422).json(err));
-  // }
+  deleteCategory: function (req, res) {
+    const updateCategories = { $pull: { levelOne: {category: req.body.category}, categories: req.body.category }};
+    db.User
+      .update({ username: req.body.username }, updateCategories, { multi: true })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
 };
